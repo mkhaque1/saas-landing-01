8599bb273e1848581a7747b5e99017e69b976d89
mkhaque1 <mkhaque29@gmail.com>
2024-07-13 23:14:02 +0200

    bug fix for features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,6 +1,11 @@
 import CardIcon from "../assets/icons/box 1.svg";
 
-const features = [
+interface Feature {
+  title: string;
+  description: string;
+}
+
+const features: Feature[] = [
   {
     title: "Integration Ecosystem",
     description:
@@ -18,28 +23,31 @@ const features = [
   },
 ];
 
-export const Features = () => {
+export const Features: React.FC = () => {
   return (
-    <div className=" bg-black text-white">
-      <div className=" container py-24 text-center">
-        <div className=" flex flex-col justify-center items-center mb-5">
-          <h1 className=" text-6xl font-bold tracking-tighter">
+    <div className="bg-black text-white">
+      <div className="container py-24 text-center">
+        <div className="flex flex-col justify-center items-center mb-5">
+          <h1 className="text-6xl font-bold tracking-tighter">
             Everything you need
           </h1>
-          <p className=" py-5 w-full md:w-[576px] md:text-xl">
+          <p className="py-5 w-full md:w-[576px] md:text-xl">
             Enjoy customizable lists, team work tools, and smart tracking all in
             one place. Set tasks, get reminders, and see your progress simply
             and quickly.
           </p>
         </div>
-        <div className=" card md:flex md:gap-3 xl:gap-5 ">
-          {features.map(({ title, description }) => (
-            <div className=" flex flex-col items-center gap-6 text-center mb-9 border border-white/30 rounded-2xl py-5 md:py-10 mx-10 md:mx-3">
-              <div className=" ">
-                <CardIcon className=" text-zinc-500 w-12 h-12 rounded-lg p-3 bg-slate-200 " />
+        <div className="card md:flex md:gap-3 xl:gap-5">
+          {features.map(({ title, description }, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center gap-6 text-center mb-9 border border-white/30 rounded-2xl py-5 md:py-10 mx-10 md:mx-3"
+            >
+              <div>
+                <CardIcon className="text-zinc-500 w-12 h-12 rounded-lg p-3 bg-slate-200" />
               </div>
-              <h3 className=" font-bold">{title}</h3>
-              <p className=" text-slate-300 px-10">{description}</p>
+              <h3 className="font-bold">{title}</h3>
+              <p className="text-slate-300 px-10">{description}</p>
             </div>
           ))}
         </div>
