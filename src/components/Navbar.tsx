e00a4b47c7d909245674d0b3cff5c7d7736b3302
mkhaque1 <mkhaque29@gmail.com>
2024-07-13 12:38:54 +0200

    nav adjusted

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
     <div className="bg-black">
       <div className=" container flex justify-between px-5 py-4 ">
         <div className=" relative">
-          <div className=" absolute w-full blur-md top-0 bottom-0 bg-[linear-gradient(to_right,#FCD6FF,#29D8FF,#FFFD80,#F89ABF)]"></div>
+          <div className=" absolute w-full blur-lg top-0 bottom-2 bg-[linear-gradient(to_right,#FCD6FF,#29D8FF,#FFFD80,#F89ABF)]"></div>
           <Image src={logoimg} alt="logo" className=" w-10 h-10 relative" />
         </div>
 
@@ -25,7 +25,7 @@ export const Navbar = () => {
           ))}
           <button
             type="button"
-            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br  shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           >
             Book Demo
           </button>
